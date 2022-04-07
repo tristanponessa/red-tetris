41dd7ae0282c9235f699a5f5788e23614ba8cc98
tristanponessa <trps_42@protonmail.com>
2022-04-07 23:07:04 +0200

    bunch of project files i forgot to push like package.json public src

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Link from './components/Link';
+
+function App() {
+  return (
+    <div>
+      Hi!
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Link.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const Link = () => {
+
+  return (
+    <>
+        <p>learn react</p>
+    </>
+  );
+};
+
+export default Link;
