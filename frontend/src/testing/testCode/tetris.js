26c6995f8772435ccc936aacf4fb81eeb034c197
tristanponessa <trps_42@protonmail.com>
2022-04-09 00:50:11 +0200

    reworked on tetris.js backend

--- frontend/src/testing/testCode/tetris.js
+++ frontend/src/testing/testCode/tetris.js
@@ -1,175 +1,201 @@
-/*
-    a blueprint is something static that you copy that will be dynamic
-*/
+class Player {
+
+    static bluePrint = {
+        PlayerName : null,
+        board : Array.from(Array(20), ()=>new Array(10).fill(null)),
+        blockedLines : 0,
+        score : 0,
+        piece : null,
+        history : []
+    }
+
+    constructor (playerName) {
+        this.data = {...Player.bluePrint};
+        this.data.PlayerName = playerName;
+    }
+}
+
+class Board {
+
+    /*
+        '-' nothing there
+        'X' indestructible
+    */
+    constructor() {
+        this.board =  Array.from(Array(20), ()=>new Array(10).fill(null))
+
+    }
+
+    clean() {}
 
 
-/*
-Array.from(Array(20), () => new Array(10).fill(null));
-wrong : Array(2).fill(Array(4));      have been copied by reference. That means a arr[0][0] = 'foo' would actually change two rows instead of one.
-
-*/
+}
 
 
-const playerBlueprint = {
-    PlayerName : null,
-    board : Array.from(Array(20), ()=>new Array(10).fill(null)),
-    blockedLines : 0,
-    score : 0,
-    piece : null,
-    history : []
-};
+class Piece {
 
-const liveGameBlueprint = {
-    turn : 0,
-    gravity: 10, /* pull interval seconds */
-    nbPlayers: 0
-};
+    static tetriminioes = {
+            rotationKeys : ['horizontal', 'd90', 'vertical', 'd180'],
+            /* ==== */
+            I : {
+                nickname: 'line',
+                start: 0,
+                color: 'cyan',
+                offsetsDrawing: [
+                    ['-', 'D', 'B', '-'],
+                    ['A', 'AD', 'AB', 'A'],
+                    ['C', 'CD', 'CB', 'C'],
+                    ['-', 'D', 'B', '-']
+                ],
+                OffsetsStatic: {
+                    A:[{y:1, x:0}, {y:1, x:1}, {y:1, x:2}, {y:1, x:3}],
+                    B:[{y:0, x:2}, {y:1, x:2}, {y:2, x:2}, {y:3, x:2}],
+                    C:[{y:2, x:0}, {y:2, x:1}, {y:2, x:2}, {y:2, x:3}],
+                    D:[{y:0, x:1}, {y:1, x:1}, {y:2, x:1}, {y:3, x:1}]
+                },
+                OffsetsDynamic: {
+                    get horizantal() { return offsetAuto(this.offsetsDrawing, 'A');},
+                    get d90()      { return offsetAuto(this.offsetsDrawing, 'B');},
+                    get vertical() { return offsetAuto(this.offsetsDrawing, 'C');},
+                    get d180() { return offsetAuto(this.offsetsDrawing, 'D');}
+                }
+            },
 
+            /*    =    */
+            /*   ===     */
+            T : null,
+            /*    ==    */
+            /*   ==     */
+            S: null, 
+            /*  ==      */
+            /*   ==     */
+            Z: null,
+            /*   =    */
+            /*   ===     */
+            J : null,
+            /*     =   */
+            /*   ===     */
+            L : null,
+            /*   ==    */
+            /*   ==     */
+            O : null,
+            /* XXXXXXXXXX */
+            Indestructible : 'X',
+            empty: '-'
 
+        }
 
-/* -------------------------------------------------------------------------------  */
+        constructor(tetriminosBlueprint) {
+        
+            this.name = this.randomProperty(tetriminosBlueprint, 'key');
+            this.curBlueprint = tetriminosBlueprint[this.name]; //ref
+            this.rotKeys = Object.keys(this.curBlueprint.OffsetsDynamic);
+            this.curRotation = 'horizantal';
+            this.curOffsets = this.curBlueprint.OffsetsDynamic[this.curRotation] //copy {00,11,21,01}
+            this.curPos = {y: 0, x:4}; //piece is placed around your pointer
+            
+        }
+    
+        rotateRight() {
+            this.curRotation = this.nextRotation();
+            this.curOffsets = this.curBlueprint.OffsetsDynamic[this.curRotation];
+        }
+    
+        move(board, direction) {
 
+            oldOffsets = this.oldCords //ref copy? we need copy
+            oldPos = this.curPos; //ref copy?
 
-/* search for cords from drawing of tetriminos*/
-function offsetAuto(offsetDarray, letter) {
-    const offsets = [{y:null, x:null}, {y:null, x:null}, {y:null, x:null}, {y:null, x:null}];
-    let i = 0;
-    for (let y = 0; y < offsetDarray.length; y++) 
-        for (let x = 0; x < offsetDarray[y].length; x++) {
-            if (letter in offsetDarray[y][x]) {
-                offsets[i].y = y;
-                offsets[i].x = x;
-                i++;
+            if (!this.landed()) {
+                if (direction === 'up')
+                    this.rotateRight();
+                if (direction === 'down')
+                    this.curPos.y += 1;
+                if (direction === 'left')
+                    this.curPos.x -= 1;
+                if (direction === 'right')
+                    this.curPos.x += 1;
+                
+                /* clean before placing piece */
+                for (oldcord of this.oldOffsets) {
+                    board[oldcord + oldPos.y][oldcord + oldPos.x] = this.tetriminioes.empty;
+                }
+                for (cord of this.curOffsets) {
+                    /* if in contact */
+                    board[this.curPos + cord.y][this.curPos + cord.x] = this.name;
+                }
+        }
+        
+        landed(board) {
+            for (cord of this.curOffsets) {
+                if (board[cord.y + this.curPos][cord.x + this.curPos] !== this.tetriminioes.empty &&
+                    this.offsetsDrawing[cord.y][cord.x] !== this.tetriminioes.empty)
+                    return ?;
             }
-    }
-    return offsets;
+        }
+    
+        /* static fns dont use this */
+    
+        randomProperty(obj, which='val') {
+            const keys = Object.keys(obj);
+            rkey = keys[ keys.length * Math.random() << 0];
+            return which === 'key' ? rkey : obj[rkey];
+        };
+        
+        nextRotation() {
+            let i = this.rotKeys.findIndex(this.curRotation);
+            i++;
+            if (i > keys.length - 1)
+                i = 0;
+            return keys[i];
+        }
+
+        /* search for cords from drawing of tetriminos*/
+        offsetAuto(offsetDarray, letter) {
+            const offsets = [{y:null, x:null}, {y:null, x:null}, {y:null, x:null}, {y:null, x:null}];
+            let i = 0;
+            for (let y = 0; y < offsetDarray.length; y++) 
+                for (let x = 0; x < offsetDarray[y].length; x++) {
+                    if (letter in offsetDarray[y][x]) {
+                        offsets[i].y = y;
+                        offsets[i].x = x;
+                        i++;
+                    }
+            }
+            return offsets;
+        }
 }
 
 
-
-/* offsets represents all positions rotations in a box 
-    when player moves, we simply apply offsets of current ratation letter to player cintrol center
+/*
+** @property {Player} players
 */
-const tetriminosBlueprint = {
 
-    rotationKeys : ['horizontal', 'd90', 'vertical', 'd180'],
-    /* ==== */
-    I : {
-        nickname: 'line',
-        start: 0,
-        color: 'cyan',
-        offsetsDrawing: [
-            [null, 'D', 'B', null],
-            ['A', 'AD', 'AB', 'A'],
-            ['C', 'CD', 'CB', 'C'],
-            [null, 'D', 'B', null]
-        ],
-        OffsetsStatic: {
-            A:[{y:1, x:0}, {y:1, x:1}, {y:1, x:2}, {y:1, x:3}],
-            B:[{y:0, x:2}, {y:1, x:2}, {y:2, x:2}, {y:3, x:2}],
-            C:[{y:2, x:0}, {y:2, x:1}, {y:2, x:2}, {y:2, x:3}],
-            D:[{y:0, x:1}, {y:1, x:1}, {y:2, x:1}, {y:3, x:1}]
-        },
-        OffsetsDynamic: {
-             get horizantal() { return offsetAuto(this.offsetsDrawing, 'A');},
-             get d90()      { return offsetAuto(this.offsetsDrawing, 'B');},
-             get vertical() { return offsetAuto(this.offsetsDrawing, 'C');},
-             get d180() { return offsetAuto(this.offsetsDrawing, 'D');}
-         }
-    },
+class Game {
 
-    /*    =    */
-    /*   ===     */
-    T : null,
-    /*    ==    */
-    /*   ==     */
-    S: null, 
-    /*  ==      */
-    /*   ==     */
-    Z: null,
-    /*   =    */
-    /*   ===     */
-    J : null,
-    /*     =   */
-    /*   ===     */
-    L : null,
-    /*   ==    */
-    /*   ==     */
-    O : null,
-    /* XXXXXXXXXX */
-    Indestructible : null
-};
-
-
-class Tetriminos {
-    constructor(tetriminosBlueprint) {
-        
-        this.name = this.randomProperty(tetriminosBlueprint, 'key');
-        this.curBlueprint = tetriminosBlueprint[this.name]; //ref
-        this.rotKeys = Object.keys(this.curBlueprint.OffsetsDynamic);
-        this.curRotation = 'horizantal';
-        this.curOffsets = this.curBlueprint.OffsetsDynamic[this.curRotation] //copy {00,11,21,01}
-        this.curPos = {y: 0, x:4}; //piece is placed around your pointer
-        
+    static bluePrint = {
+        turn : 0,
+        gravity: 10, /* pull interval seconds */
+        players: []
     }
 
-    rotateRight() {
-        this.curRotation = this.nextRotation();
-        this.curOffsets = this.curBlueprint.OffsetsDynamic[this.curRotation];
+    constructor(playerNames, gravity) {
+        this.data = {...this.bluePrint};
+        this.data.gravity = gravity;
+        for (p of playerNames)
+            this.data.players.push(new Player(p));
     }
-
-    move(direction) {
-        if (!this.landed()) {
-            if (direction === 'up')
-                this.rotateRight();
-            if (direction === 'down')
-                this.curPos.y += 1;
-            if (direction === 'left')
-                this.curPos.x -= 1;
-            if (direction === 'right')
-                this.curPos.x += 1;
-        }
-    }
-
-    draw(board) {
-        for (cord of this.curOffsets) {
-
-            /* if in contact */
-
-            /* if all ok */
-            board[this.curPos + cord.y][this.curPos + cord.x] = this.name;
-        }
-    }
-
-    landed(board, y, x) {
-        copyboard = [...board];
-        for (cord of this.curOffsets) {
-
-            if ((this.curBlueprint.offsetsDrawing[this.curPos.y][this.curPos.x] !== null) && \
-                    )
-                 board[this.curPos + cord.y][this.curPos + cord.x] = this.name;
-        }
-    }
-
-    /* static fns dont use this */
-
-    randomProperty(obj, which='val') {
-        const keys = Object.keys(obj);
-        rkey = keys[ keys.length * Math.random() << 0];
-        return which === 'key' ? rkey : obj[rkey];
-    };
-    
-    nextRotation() {
-        let i = this.rotKeys.findIndex(this.curRotation);
-        i++;
-        if (i > keys.length - 1)
-            i = 0;
-        return keys[i];
-    }
-    
-
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -215,6 +241,8 @@ function keyPress(key, player) {
 
 function runGame(liveGame, player) {
 
+    player = Plyaer()
+
     //async
     setInterval(pullDown(player), liveGame.gravity);
 
@@ -242,13 +270,19 @@ function runGame(liveGame, player) {
 
 //TeST with terminal   launch with node.js
 
+function initGame1() {
+    return {
+        player : Player(),
+        game : Game(),
 
+    }
+}
 
 
 
 function Main() {
     initGame();
-    runGame():
+    runGame();
 }
 
 Main();
