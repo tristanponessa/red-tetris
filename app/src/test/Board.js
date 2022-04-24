73d6296c3061031763586042cc33af3bb2e0f03e
tristanponessa <trps_42@protonmail.com>
2022-04-24 23:57:36 +0200

    working on placing pieces on board

--- app/src/test/Board.js
+++ app/src/test/Board.js
@@ -2,13 +2,14 @@ import { Piece } from './Piece';
 
 export class Board {
 
-    constructor() {
+    constructor(pieceLetter) {
         this.y = 20;
         this.x = 10;
         this.startPos = {y: 0, x: Math.trunc(this.x / 2)} // in case we wanna change x to ex.11 in the futur, remove fractional part
         this.invisibleZoneRangeMaxY = 4; //cause the longest piece I is height 4
         this.curBoard = this.newBoard();
-        this.curPiece = undefined; //new Piece(pieceLetter === undefined ? undefined : pieceLetter);
+        this.curPiece = new Piece(pieceLetter === undefined ? null : pieceLetter);
+        this.playerPos = {...this.startPos};
     }
 
     newBoard() {
@@ -16,10 +17,19 @@ export class Board {
                     () => new Array(this.x).fill(Piece.tetriminoes.empty));
     }
 
+    placeCurPiece(pos) {
+
+        let r = {state: true, cords: null};
+
+        const prev
+        this.playerPos = pos;
+
+        
+
+    }
+
     /*dropNewPiece(pieceLetter) {
         this.curPiece = new Piece(pieceLetter === undefined ? undefined : pieceLetter);
-
-
     }*/
 }
 
--- app/src/test/tetris.test.js
+++ app/src/test/tetris.test.js
@@ -83,35 +83,66 @@ test('place pieces on Baord', () => {
         in all their start pos : rotation up
         everytime error occurs curPiece keeps its last position
         we dont have to remember where the peices are so no draw fn or table
-        we just check this boardX.curPiece.pos  => board.playerPos + curPiece.offsets
-    */
-    let bPieceI = new Board();
-    let bPieceL = new Board();
-    const pieces = [new Piece('I'), new Piece('L')]; //new Array(names.length).fill(undefined).map(e => e = new Piece(e));
-    bPieceI.curPiece = pieces[0];
-    bPieceL.curPiece = pieces[1];
+        we just check this boardX.curPiece.pos  => board.playerPos + curPiece.offsets   [y x * 4]
 
-    let r; //all expects use these
-    
+        playerPos = y x on board  where arrow is
+        these board pos are the equivalent of player going directly there (if even mot possible)
+
+        we dont care about rotation cause it doesnt matter if I left or Z up is outborders, we wanna test if we test one part of it outside
+    */
+
     const outOfZone1 = {y: -1, x: 0};
     const outOfZone2 = {y: 0, x: -1};
     const outOfZone3 = {y: -1, x: -1};
     const outOfZone4 = {y: new Board().y, x: new Board().x}; //start at 0 so overlap 1
     const outOfZonePieceI = {y: new Board().y - 3, x: 0}; //one part should be outside bottom y maxY + 1 x 0
     const outOfZonePieceL = {y: 0, x: 0}; //one part should be outside 2 lefts 
-
     const sameZoneAndOutOfZonePieceI = [{y: new Board().y - 2, x: 0}, {y: new Board().y - 3, x: 0}]
-
     const sameZonePieceI1 = [{y: 0, x: 0}, {y : 0, x : 0}] //with 2 Is
     const sameZonePieceI2 = [{y: 0, x: 0}, {y : 3, x : 0}] //with 2 Is
-
     const validZonePieceI1 = [{y: 0, x: 0}, {y : 4, x : 4}] //with 2 Is top of eachother
 
-    //tewst I and L
+    function ExpectMove(res, boardX, attemptPos, noConditionMovePieceCords) {
+        
+        let b = boardX.includes('new') ? new Board(boardX[boardX.length - 1]) : boardX;
+        const prevPieceCords = b.curPiece.cords;
+        const r = b.placeCurPiece(attemptPos);
+
+        expect(r.state).toStrictEqual(res);
+        expect(r.cords).toStrictEqual(noConditionMovePieceCords); //if error dont move  r.cords contains if moved despite error
+        expect(b.curPiece.cords).toStrictEqual(res === true ? noConditionMovePieceCords : prevPieceCords);
+
+        return b; //for chaining
+    }
+
+    //this is internal fn to 
+    /*static Piece.GetCords() { 
+        return this.offsets[this.curRotation] + this.playerPos;
+    }*/
+
+    const offsI = new Piece('I').offsets.up;
+    const offsL = new Piece('L').offsets.up;
+
+    //test I and L
+    ExpectMove(false, 'newI', outOfZone1, [{y: outOfZone1[0].y + offsI[0].y, x: outOfZone1[0].x + offsI[0].x},
+                                            {y: outOfZone1[1].y + offsI[1].y, x: outOfZone1[1].x + offsI[1].x},
+                                            {y: outOfZone1[2].y + offsI[2].y, x: outOfZone1[2].x + offsI[2].x},
+                                            {y: outOfZone1[3].y + offsI[3].y, x: outOfZone1[3].x + offsI[3].x}])
+
+
+
+
+
+    /*/
     for (let b of [bPieceI, bPieceL]) {
         
+        
+
         let r1 = b.placeCurPiece(outOfZone1);
         expect(r1).toStrictEqual(false); expect(b.curPiece.pos).toStrictEqual(null);
+
+        
+
         let r2 = b.placeCurPiece(outOfZone2);
         expect(r2).toStrictEqual(false); expect(b.curPiece.pos).toStrictEqual(null);
         let r3 = b.placeCurPiece(outOfZone3);
@@ -121,33 +152,7 @@ test('place pieces on Baord', () => {
 
     }
 
-    function ExpectSuccessAndMovefunction(boardX, pieceLetter, firstPos, attemptPos, failedPieceCords) {
-        
-        let b = boardX === 'new' ? new Board(pieceLetter) : boardX;
-
-        r = bPieceI.placeCurPiece(attemptPos); //error
-
-        expect(r.state).toStrictEqual(true);
-        expect(r.curPos).toStrictEqual(firstPos); //new pos
-        expect(bPieceI.curPiece.pos).toStrictEqual(failedPieceCords); 
-
-        return b; //for chaining
-    }
-
-    ExpectFailAndNoMove('I', null, )
-
-    function ExpectFailAndNoMove(boardX, pieceLetter, firstPos, attemptPos, failedPieceCords) {
-        
-        let b = boardX === 'new' ? new Board(pieceLetter) : boardX;
-
-        r = bPieceI.placeCurPiece(attemptPos); //error
-
-        expect(r.state).toStrictEqual(false);
-        expect(r.cords).toStrictEqual(failedPieceCords); //if error dont move  r.cords contains if moved despite error
-        expect(bPieceI.curPiece.cords).toStrictEqual(failedPieceCords); 
-
-        return b; //for chaining
-    }
+    
 
     //test piece I
     r = bPieceI.placeCurPiece(outOfZonePieceI); //error
@@ -187,16 +192,18 @@ test('place pieces on Baord', () => {
     r = bPieceL.placeCurPiece(outOfZonePieceL);
     expect(r).toStrictEqual(true);
     expect(bPieceI.curPiece.pos).toStrictEqual(validZonePieceI1);
+    */
 });
 
-test('pieces start', () => { 
+
+/*test('pieces start', () => { 
     /*
     *  the 4 top cols are not visible 
     * cause 1st position is up rotation 
     *  cause the longest piece I is height 4
     *  so it appears to slowly appear
     *
-    */
+    *//*
     const b = new Board();
     const startX = b.startPos.y;
     const startY = b.startPos.x;
@@ -215,7 +222,7 @@ test('pieces start', () => {
         expect(b.curPiece.left.x).toStrictEqual(startX);
         expect(b.curPiece.right.x).toBeLessThanOrEqual(b.x);
     }
-});
+});*/
 /*
 test('pieces exist', () => {
 
